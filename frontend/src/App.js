de26be44debc7144e8ec65d1949eccf59a9f2826
james chandler <80052602+jameschandler1@users.noreply.github.com>
2026-04-09 21:13:31 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    fetch("http://18.191.118.78:3001/api/hello")
+    fetch("http://18.191.118.78:3000/api/hello")
       .then((response) => response.json())
       .then((data) => setMessage(data.message))
       .catch((error) => console.error("Error fetching data:", error));
