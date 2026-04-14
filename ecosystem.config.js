3cdfcb2741079190cab09b8a2bc5c8c8d85594f1
james chandler <80052602+jameschandler1@users.noreply.github.com>
2026-04-14 00:53:27 -0700

    fix start up commands

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,17 +3,22 @@ module.exports = {
     {
       name: "backend",
       script: "backend/server.js",
-      cwd: "/home/ubuntu/m-project-1-wk1", // Update this to your project path
+      cwd: "/home/ubuntu/m-project-1-wk1",
       env: {
         NODE_ENV: "production",
         PORT: 3001,
+        DB_HOST: "localhost",
+        DB_USER: "app_user",
+        DB_PASSWORD: "app_password123",
+        DB_NAME: "m_project_db",
+        DB_PORT: 3306,
       },
     },
     {
       name: "frontend",
       script: "serve",
       args: "-s frontend/build -l 3000",
-      cwd: "/home/ubuntu/m-project-1-wk1", // Update this to your project path
+      cwd: "/home/ubuntu/m-project-1-wk1",
       env: {
         NODE_ENV: "production",
       },
